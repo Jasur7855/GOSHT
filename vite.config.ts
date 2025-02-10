2193e9429382163497f47968bea696e058965ba2
Fathulla Rashidov <fathullarashidovdev@gmail.com>
2025-02-10 14:49:33 +0500

    add: env configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,23 +1,30 @@
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import react from "@vitejs/plugin-react";
 
-// https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    host: true,
-  },
-  build: {
-    rollupOptions: {
-      output: {
-        manualChunks: {
-          react: ["react", "react-dom", "react-router-dom", "react-redux"],
-          redux: ["@reduxjs/toolkit"],
-          form: ["react-hook-form", "yup", "@hookform/resolvers"],
-          ui: ["styled-components", "@radix-ui/react-dropdown-menu"],
-          swiper: ["swiper", ],
+export default defineConfig(({ mode }) => {
+  // Загружаем .env файлы
+  const env = loadEnv(mode, process.cwd());
+
+  return {
+    plugins: [react()],
+    server: {
+      host: true,
+    },
+    define: {
+      'process.env': env,
+    },
+    build: {
+      rollupOptions: {
+        output: {
+          manualChunks: {
+            react: ["react", "react-dom", "react-router-dom", "react-redux"],
+            redux: ["@reduxjs/toolkit"],
+            form: ["react-hook-form", "yup", "@hookform/resolvers"],
+            ui: ["styled-components", "@radix-ui/react-dropdown-menu"],
+            swiper: ["swiper"],
+          },
         },
       },
     },
-  },
+  };
 });
